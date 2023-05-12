68edf5be6296e61744fe43bf255c1bb41ace2b66
keithjl <keithjl@mit.edu>
2023-05-11 20:28:52 -0400

    section analysis

---
+++ Section/Analysis.cs
@@ -0,0 +1,78 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSUtilities.Section
+{
+    internal static class Analysis
+    {
+        /// <summary>
+        /// Checks if sections are closed and planar
+        /// </summary>
+        /// <param name="curves"></param>
+        /// <returns></returns>
+        public static bool ValidCheck(List<Curve> curves)
+        {
+            for (int i = 0; i < curves.Count; i++)
+            {
+                Curve curve = curves[i];
+                if (curve.IsClosed && curve.IsPlanar())
+                {
+                    continue;
+                }
+                else
+                {
+                    return false; //if any of the curves are open or non-planar, return false
+                }
+            }
+
+            return true; //else return true
+        }
+
+        /// <summary>
+        /// Computes the total area of an arbitrary section
+        /// </summary>
+        /// <param name="sections"></param>
+        /// <returns></returns>
+        public static double TotalArea(List<Curve> sections)
+        {
+            double Atotal = 0.0;
+            for (int i = 0; i < sections.Count; i++)
+            {
+                Curve curve = sections[i];
+                var curveProp = AreaMassProperties.Compute(curve);
+                Atotal += curveProp.Area;
+            }
+
+            return Atotal;
+        }
+
+        public static BoundingBox GetBB(List<Curve> curves)
+        {
+            BoundingBox bb = new BoundingBox();
+
+            for (int i = 0; i <= curves.Count; i++)
+            {
+                bb.Union(curves[i].GetBoundingBox(true));
+            }
+
+            return bb;
+        }
+
+        public static List<double> SectionModulii(Point3d Centroid, BoundingBox bb)
+        {
+            List<double> S = new List<double>();
+            // X axis bending
+            var vxtop = bb.Corner(true, false, false) - Centroid;
+            var dxtop = Math.Sqrt(Math.Pow(vxtop.Y, 2) + Math.Pow(vxtop.Z, 2));
+            S.Add(dxtop);
+
+            var vxbot = bb.Corner(true, false, true) - Centroid;
+            var dxbot = Math.Sqrt(Math.Pow(vxbot.Y, 2) + Math.Pow(vxbot.Z, 2));
+            S.Add(dxbot);
+        }
+    }
+}
---
+++ Section/Section.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+using MathNet.Numerics;
+
+namespace DSUtilities.Section
+{
+    internal class Section
+    {
+        public double Area;
+        public Point3d Centroid;
+        public Vector3d Inertias;
+        public List<double> SectionModulii;
+        public BoundingBox BoundingBox;
+        public double E = 1;
+        public List<Curve> Solids;
+        public List<Curve> Voids;
+
+        public Section(List<Curve> curves, List<Curve> voids, double E)
+        {
+            //go/no-go
+            if (!Analysis.ValidCheck(curves)) return;
+            if (!Analysis.ValidCheck(voids)) return;
+
+            //initialize
+            Area = 0;
+            Centroid = new Point3d();
+            Solids = curves;
+            Voids = voids;
+            this.E = E;
+
+            
+            //individual properties
+            var areas = new List<double>();
+            var centroids = new List<Point3d>();
+            var inertias = new List<Vector3d>();
+
+            List<Curve> allcurves = curves.Concat(voids).ToList();
+
+            foreach (Curve curve in curves)
+            {
+                var properties = AreaMassProperties.Compute(curve);
+
+                areas.Add(properties.Area);
+                Area += properties.Area;
+
+                centroids.Add(properties.Centroid);
+                Centroid += properties.Area * properties.Centroid;
+
+                inertias.Add(properties.CentroidCoordinatesMomentsOfInertia);
+            }
+
+            foreach (Curve curve in voids)
+            {
+                var properties = AreaMassProperties.Compute(curve);
+
+                areas.Add(-properties.Area);
+                Area -= properties.Area;
+
+                centroids.Add(properties.Centroid);
+                Centroid += -properties.Area * properties.Centroid;
+            }
+
+            //get total centroid
+            Centroid /= Area;
+
+            // compound moment of inertia
+            double Ix = 0;
+            double Iy = 0;
+            double Iz = 0;
+
+            for (int i = 0; i < allcurves.Count; i++)
+            {
+                var cent2cent = Centroid - centroids[i];
+                Ix += inertias[i][0] + areas[i] * (Math.Pow(cent2cent.Y, 2) + Math.Pow(cent2cent.Z, 2));
+                Iy += inertias[i][1] + areas[i] * (Math.Pow(cent2cent.X, 2) + Math.Pow(cent2cent.Z, 2));
+                Iz += inertias[i][2] + areas[i] * (Math.Pow(cent2cent.X, 2) + Math.Pow(cent2cent.Y, 2));
+            }
+
+            Inertias = new Vector3d(Ix, Iy, Iz);
+
+            //get bounding box
+            this.BoundingBox = Analysis.GetBB(curves);
+        }
+
+
+    }
+}

cf2585e2912bfbee622c7f9a5425fa855cd99754
keithjl <keith.ja.lee@gmail.com>
2024-02-02 16:24:45 -0500

    I beams and channels

---
+++ CommonSections/IBeam.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace DSUtilities.CommonSections
+{
+    public class IBeam : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the IBeam class.
+        /// </summary>
+        public IBeam()
+          : base("IBeam", "I",
+              "Make a flanged I beam",
+              "DSutilities", "SectionMaker")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("Depth", "d", "Depth of section", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Width", "b", "Width of flange", GH_ParamAccess.item);
+            pManager.AddNumberParameter("tFlange", "tf", "Flange thickness", GH_ParamAccess.item);
+            pManager.AddNumberParameter("tWeb", "tw", "Web thickness", GH_ParamAccess.item);
+            pManager.AddPlaneParameter("Plane", "Plane", "Plane origin of section", GH_ParamAccess.item, Plane.WorldXY);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("Geometry", "Geo", "Geometry of section", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Initialize
+            double d = 0;
+            double b = 0;
+            double tf = 0;
+            double tw = 0;
+            Plane plane = Plane.WorldXY;
+
+            // assign
+            if (!DA.GetData(0, ref d)) return;
+            if (!DA.GetData(1, ref b)) return;
+            if (!DA.GetData(2, ref tf)) return;
+            if (!DA.GetData(3, ref tw)) return;
+            DA.GetData(4, ref plane);
+
+            // calculate
+            Curve curve = SectionDrawer.MakeIBeam(plane, d, b, tf, tw);
+            Brep brep = Brep.CreatePlanarBreps(curve, 1e-6)[0];
+
+            DA.SetData(0, brep);
+
+            //
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("30A649EA-A8C6-4E46-B181-7A6CBD6FE1C3"); }
+        }
+    }
+}
---
+++ CommonSections/SectionDrawer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace DSUtilities.CommonSections
+{
+    internal static class SectionDrawer
+    {
+        public static Curve MakeIBeam(Plane plane, double d, double b, double tf, double tw)
+        {
+            // extract LCS
+            Point3d O = plane.Origin;
+            Vector3d X = plane.XAxis;
+            Vector3d Y = plane.YAxis;
+
+            // make points
+            Point3d p1 = O - X * tw / 2;
+            Point3d p2 = p1 + Y * (d / 2 - tf);
+            Point3d p3 = p2 - X * (b - tw) / 2;
+            Point3d p4 = p3 + Y * tf;
+            Point3d p5 = O + Y * d / 2;
+
+            List<Point3d> quadrant = new List<Point3d> { p1, p2, p3, p4, p5 };
+
+            // make top left quadrant curve
+            PolylineCurve top_left = new PolylineCurve(quadrant);
+
+            // vertical mirror transform
+            PolylineCurve bottom_left = (PolylineCurve) top_left.Duplicate();
+
+            // flip plane
+            Plane flip_plane_1 = new Plane(O, Y);
+            Transform flip_transform_1 = Transform.Mirror(flip_plane_1);
+
+            // mirror
+            bottom_left.Transform(flip_transform_1);
+
+            // join
+            Curve left = Curve.JoinCurves(new List<PolylineCurve> { top_left, bottom_left })[0];
+
+            //copy
+            Curve right = (Curve)left.Duplicate();
+
+            // flip
+            Plane flip_plane_2 = new Plane(O, -X);
+            Transform flip_transform_2 = Transform.Mirror(flip_plane_2);
+
+            //mirror
+            right.Transform(flip_transform_2);
+
+            //join
+            Curve section = Curve.JoinCurves(new List<Curve> { left, right })[0];
+
+            return section;
+           
+        }
+
+        public static Curve MakeChannel(Plane plane, double d, double b, double t)
+        {
+            // extract LCS
+            Point3d O = plane.Origin;
+            Vector3d X = plane.XAxis;
+            Vector3d Y = plane.YAxis;
+
+            // make points
+            Point3d p1 = O - X * t / 2;
+            Point3d p2 = p1 + Y * d / 2;
+            Point3d p3 = p2 + X * b;
+            Point3d p4 = p3 - Y * t;
+            Point3d p5 = p4 - X * (b - t);
+            Point3d p6 = p5 - Y * (d / 2 - t);
+
+            PolylineCurve top = new PolylineCurve(new List<Point3d> { p1, p2, p3, p4, p5, p6 });
+
+            //mirror
+            PolylineCurve bottom = (PolylineCurve)top.Duplicate();
+            Plane flip_plane = new Plane(O, Y);
+            Transform flip_transform = Transform.Mirror(flip_plane);
+            bottom.Transform(flip_transform);
+
+            //join
+            Curve section = Curve.JoinCurves(new List<Curve> { top, bottom })[0];
+
+            return section;
+        }
+    }
+}
